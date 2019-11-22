be6bf475f4bb626342e49479627a5e076c2fec06
Adilet312 <adiletm@hotmail.com>
2019-11-22 15:35:30 -0800

    Fetching and printing data in our browser.

--- src/doctorLookUp.js
+++ src/doctorLookUp.js
@@ -15,4 +15,20 @@ export class Class_DoctorLookUp
         }
         
     }
+
+    // async getDoctorsByName(name,n_location)
+    // {
+    //     try
+    //     {
+    //         let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=${n_location}&user_key=${process.env.API_KEY}`);
+    //         let json_format_Response = await response.json();
+    //         return json_format_Response;
+    //     }
+    //     catch(error)
+    //     {
+    //         let errorMassage = "There was an error handling your request:"+error.message;
+    //         return errorMassage;
+    //     }
+        
+    // }
 }
--- src/main.js
+++ src/main.js
@@ -11,17 +11,45 @@ $(document).ready(function(){
         let medical_issue = $("#medicalIssueID").val();
         let name_doctor = $("#nameDoctorID").val();
         let doctor_Location = $("#doctorLocationID").val();
-        console.log(medical_issue,doctor_Location);
+        let listDoctors = new Class_DoctorLookUp();
+
         (async()=>{
-            let listDoctors = new Class_DoctorLookUp();
+            
             const response_json = await listDoctors.getList_Doctors(medical_issue,doctor_Location);
             //console.log(response_json);
             getElements(response_json);
         })();
+        // (async()=>{
+            
+        //     const response_json = await listDoctors.getList_Doctors(medical_issue,doctor_Location);
+        //     //console.log(response_json);
+        //     getElements(response_json);
+        // })();
+
         function getElements(response)
         {
-            console.log(response);
-            $("#outputID").text(response.meta.practices.name);
+            let results ="";
+            for(let idx=0; idx<response.data.length; idx++)
+            {
+
+                results+="First name: "+response.data[idx].profile.first_name +
+                       "; Last name: "+response.data[idx].profile.last_name + 
+                       "; Address: ["+response.data[idx].practices[0].visit_address.street+
+                       ", "+response.data[idx].practices[0].visit_address.city+
+                       ", "+response.data[idx].practices[0].visit_address.state+
+                       ", "+response.data[idx].practices[0].visit_address.zip+
+                       "]; Website: "+response.data[idx].profile.image_url;
+                       if(response.data[idx].practices[0].accepts_new_patients===true)
+                       {
+                        results+="; Accepting a new patients: YES"+"<br><br>";
+                       }
+                       else
+                       {
+                        results+="; Accepting a new patients: NO"+"<br><br>";
+                       }
+                
+            }
+            $("#outputID").html(results);
         }
     });
 });
