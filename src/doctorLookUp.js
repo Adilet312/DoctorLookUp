c8fe340e8bcc185296586bbc02d066db5356c2b8
Adilet312 <adiletm@hotmail.com>
2019-11-22 23:14:17 -0800

    Work with two options one: first is searching doctors by symptom ,second one searching specific doctor by full name.

--- src/doctorLookUp.js
+++ src/doctorLookUp.js
@@ -16,19 +16,19 @@ export class Class_DoctorLookUp
         
     }
 
-    // async getDoctorsByName(name,n_location)
-    // {
-    //     try
-    //     {
-    //         let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=${n_location}&user_key=${process.env.API_KEY}`);
-    //         let json_format_Response = await response.json();
-    //         return json_format_Response;
-    //     }
-    //     catch(error)
-    //     {
-    //         let errorMassage = "There was an error handling your request:"+error.message;
-    //         return errorMassage;
-    //     }
+    async getDoctorsByName(first_name,last_name,choosen_location)
+    {
+        try
+        {
+            let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?first_name=${first_name}&last_name=${last_name}&location=${choosen_location}&user_key=${process.env.API_KEY}`);
+            let json_format_Response = await response.json();
+            return json_format_Response;
+        }
+        catch(error)
+        {
+            let errorMassage = "There was an error handling your request:"+error.message;
+            return errorMassage;
+        }
         
-    // }
+    }
 }
--- src/main.js
+++ src/main.js
@@ -9,47 +9,122 @@ $(document).ready(function(){
         event.preventDefault();
         //const location ="Portland";
         let medical_issue = $("#medicalIssueID").val();
-        let name_doctor = $("#nameDoctorID").val();
+        let first_name = $("#doctorFirstNameID").val();
+        let last_name = $("#doctorLastNameID").val();
         let doctor_Location = $("#doctorLocationID").val();
         let listDoctors = new Class_DoctorLookUp();
 
-        (async()=>{
-            
-            const response_json = await listDoctors.getList_Doctors(medical_issue,doctor_Location);
-            //console.log(response_json);
-            getElements(response_json);
-        })();
-        // (async()=>{
-            
-        //     const response_json = await listDoctors.getList_Doctors(medical_issue,doctor_Location);
-        //     //console.log(response_json);
-        //     getElements(response_json);
-        // })();
-
-        function getElements(response)
+        // if location is empty string then location  will 'be wa-seattle' by default.
+        if(!doctor_Location){ doctor_Location = "or-portland";}
+        
+    
+        if(medical_issue!==''  && first_name==='' && last_name==='')
         {
-            let results ="";
-            for(let idx=0; idx<response.data.length; idx++)
-            {
-
-                results+="First name: "+response.data[idx].profile.first_name +
-                       "; Last name: "+response.data[idx].profile.last_name + 
-                       "; Address: ["+response.data[idx].practices[0].visit_address.street+
-                       ", "+response.data[idx].practices[0].visit_address.city+
-                       ", "+response.data[idx].practices[0].visit_address.state+
-                       ", "+response.data[idx].practices[0].visit_address.zip+
-                       "]; Website: "+response.data[idx].profile.image_url;
-                       if(response.data[idx].practices[0].accepts_new_patients===true)
-                       {
-                        results+="; Accepting a new patients: YES"+"<br><br>";
-                       }
-                       else
-                       {
-                        results+="; Accepting a new patients: NO"+"<br><br>";
-                       }
+            let output="";
+            console.log(medical_issue);
+            (async()=>{
                 
-            }
-            $("#outputID").html(results);
+                const response = await listDoctors.getList_Doctors(medical_issue,doctor_Location);
+                for(let idx=0; idx<response.data.length; idx++)
+                    {
+
+                        output+="First name: "+response.data[idx].profile.first_name +
+                            "; Last name: "+response.data[idx].profile.last_name + 
+                            "; Address: ["+response.data[idx].practices[0].visit_address.street+
+                            ", "+response.data[idx].practices[0].visit_address.city+
+                            ", "+response.data[idx].practices[0].visit_address.state+
+                            ", "+response.data[idx].practices[0].visit_address.zip+
+                            "]; Website: "+response.data[idx].profile.image_url;
+                            if(response.data[idx].practices[0].accepts_new_patients===true)
+                            {
+                                output+="; Accepting a new patients: YES"+"<br><br>";
+                            }
+                            else
+                            {
+                                output+="; Accepting a new patients: NO"+"<br><br>";
+                            }
+                        
+                    }
+                    $("#outputID").html(output);
+
+            })();
         }
+        
+        else if(medical_issue==='' && first_name!=='' && last_name!=='')
+        {
+        
+            let results="";
+            (async()=>{
+                
+                const response = await listDoctors.getDoctorsByName(first_name,last_name,doctor_Location);
+                for(let idx=0; idx<response.data.length; idx++)
+                    {
+                        if(response.data[idx].profile.last_name===last_name)
+                        {
+
+                        results+="First name: "+response.data[idx].profile.first_name +
+                            "; Last name: "+response.data[idx].profile.last_name + 
+                            "; Address: ["+response.data[idx].practices[0].visit_address.street+
+                            ", "+response.data[idx].practices[0].visit_address.city+
+                            ", "+response.data[idx].practices[0].visit_address.state+
+                            ", "+response.data[idx].practices[0].visit_address.zip+
+                            "]; Website: "+response.data[idx].profile.image_url;
+                            if(response.data[idx].practices[0].accepts_new_patients===true)
+                            {
+                                results+="; Accepting a new patients: YES"+"<br><br>";
+                                
+                            }
+                            else
+                            {
+                                results+="; Accepting a new patients: NO"+"<br><br>";
+                            }
+                            
+                        }
+                        
+                    }
+                    $("#outputID").html(results);
+            })();
+            
+        }
+        else if(medical_issue==='' && first_name==='' && last_name==='')
+        {
+            let  errorMassage = "Please enter first, last name, and location or sypmtom and location to get list of doctors! ";
+            $("#outputID").html(errorMassage);
+        }
+        else if((medical_issue===''&& first_name==='')||(medical_issue===''&& last_name===''))
+        {
+            let  errorMassage = "";
+            if(last_name)
+            {
+                errorMassage = "Please enter first name!";
+            }
+            else
+            {
+                errorMassage = "Please enter last name!";
+            }
+            $("#outputID").html(errorMassage);
+        }
+        else if(medical_issue!=='' && first_name!=='' && last_name==='')
+        {
+            let  errorMassage = "Please choose searching doctors by symptom or by first and last name!";
+            $("#outputID").html(errorMassage);
+
+        }
+        else if(medical_issue!=='' && last_name!=='' && first_name==='')
+        {
+            let  errorMassage = "Please choose searching doctors by symptom or by first and last name!";
+            $("#outputID").html(errorMassage);
+
+        }
+        else if(medical_issue!=='' && first_name!=='' && last_name!=='')
+        {
+            let  errorMassage = "Please choose searching doctors by symptom or searching specific doctor by first and last name!";
+            $("#outputID").html(errorMassage); 
+        }
+       
+        
+       
+
+        
     });
 });
